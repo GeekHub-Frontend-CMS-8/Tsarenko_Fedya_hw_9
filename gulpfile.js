04e24a2a4fa47112a404800d4730c4e38eb089fa
Maksym Tokar <maksmto@interlink-ua.com>
2018-11-06 22:41:14 +0200

    fix names

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,28 @@
-// var gulp = require("gulp");
-// var watch = require('gulp-watch');
-// var browserSync = require('browser-sync').create();
-//
-// gulp.task('browser-sync', function() {
-// 	browserSync.init({
-// 		server: {
-// 			baseDir: "./build"
-// 		}
-// 	});
-// });
-//
-// gulp.task('watch', function () {
-// 	return watch(['assets/**/*.css', 'assets/!**/!*.html'], { ignoreInitial: false })
-// 		.pipe(gulp.dest('build'));
-// });
-
-
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
-// var sass        = require('gulp-sass');
+var sass        = require('gulp-sass');
 
-gulp.task('serve',  function() {
-
-	browserSync.init({
-		server: "./assets"
-	});
-
-	gulp.watch("assets/**/*.css").on('change', browserSync.reload);;
-	gulp.watch("assets/**/*.html").on('change', browserSync.reload);
+gulp.task('sass', function () {
+    return gulp.src('assets/sass/**/*.sass')
+        .pipe(sass())
+        .pipe(gulp.dest('build/css'))
+		.pipe(browserSync.reload({stream: true}));
 });
 
-//
-// gulp.task('sass', function() {
-// 	return gulp.src("assets/**/*.scss")
-// 		.pipe(sass())
-// 		.pipe(gulp.dest("./build/css"))
-// 		.pipe(browserSync.stream());
-// });
-//
-// gulp.task('default', ['serve']);
+gulp.task("html", function () {
+	return gulp.src("assets/**/*.html")
+		.pipe(gulp.dest("build"))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task("watch", [ 'sass', "html"], function () {
+    browserSync.init({
+        server: "./build",
+		notify: false,
+		ui: {
+        	port: 9999
+		}
+    });
+    gulp.watch('assets/sass/**/*.sass', ["sass"]);
+    gulp.watch('assets/**/*.html' , ['html']);
+});
